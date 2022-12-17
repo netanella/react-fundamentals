e25b3841dd2aeade72a8676fddd45f714dd6a941
Netanella <netanella@monalabs.io>
2022-12-17 20:30:04 +0200

    ex 6

--- src/exercise/06.js
+++ src/exercise/06.js
@@ -9,7 +9,7 @@ function UsernameForm({onSubmitUsername}) {
   // `event.preventDefault()` to prevent the default behavior of form submit
   // events (which refreshes the page).
   // 📜 https://developer.mozilla.org/en-US/docs/Web/API/Event/preventDefault
-  //
+
   // 🐨 get the value from the username input (using whichever method
   // you prefer from the options mentioned in the instructions)
   // 💰 For example: event.target.elements[0].value
@@ -19,11 +19,17 @@ function UsernameForm({onSubmitUsername}) {
 
   // 🐨 make sure to associate the label to the input.
   // to do so, set the value of 'htmlFor' prop of the label to the id of input
+
+  const handleSubmit = event => {
+    event.preventDefault()
+    const userName = event.target.elements.usernameInput.value
+    onSubmitUsername(userName)
+  }
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <div>
-        <label>Username:</label>
-        <input type="text" />
+        <label htmlFor="usernameInput">Username:</label>
+        <input type="text" id="usernameInput" />
       </div>
       <button type="submit">Submit</button>
     </form>
